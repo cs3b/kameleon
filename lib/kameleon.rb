df05f4af2f19bc2b77c85e6e666fef985c538abb
Michal Czyz <michalczyz@gmail.com>
2011-12-08 21:03:11 +0100

    Abstract User - capybara session refactoring

--- lib/kameleon.rb
+++ lib/kameleon.rb
@@ -1,3 +1,5 @@
+require 'kameleon/session/capybara'
+
 require 'kameleon/user/abstract'
 require 'kameleon/user/base'
 require 'kameleon/user/guest'
--- lib/kameleon/user/abstract.rb
+++ lib/kameleon/user/abstract.rb
@@ -4,36 +4,7 @@ module Kameleon
       attr_accessor :rspec_world
       attr_accessor :session
 
-
-      def change_session(session_name)
-        @session = find_or_create_session(session_name) ||
-            current_session
-      end
-
-      private
-
-      def find_or_create_session(session_name)
-        find_session(session_name) ||
-            create_session(session_name)
-      end
-
-      def find_session(session_name)
-        session_pool[session_name]
-      end
-
-      def create_session(session_name)
-        Capybara::Session.new(Capybara.current_driver, Capybara.app).tap do |session|
-          session_pool[session_name] = session
-        end if session_name
-      end
-
-      def session_pool
-        Capybara.send(:session_pool)
-      end
-
-      def current_session
-        Capybara.current_session
-      end
+      include Kameleon::Session::Capybara
 
     end
   end
