fc929c3a9304a50d7dd8a716c631532d530674e8
Michal Czyz <michalczyz@gmail.com>
2011-12-09 16:32:32 +0100

    let's move initialization to abstract class

--- lib/kameleon/user/guest.rb
+++ lib/kameleon/user/guest.rb
@@ -1,14 +1,6 @@
 module Kameleon
   module User
     class Guest < Abstract
-      def initialize(rspec_world, options={})
-        @rspec_world = rspec_world
-        @driver_name = options.delete(:driver)
-        @session_name = options.delete(:session_name)
-        @options = options
-        set_session
-        session.visit('/') if load_homepage?
-      end
     end
   end
 end
