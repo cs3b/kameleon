0a4f5d4115c2b19ef84eb1487ed915944ec11a20
Michal Czyz <michalczyz@gmail.com>
2011-12-10 13:55:36 +0100

    fill in checkbox

--- lib/kameleon/dsl/act.rb
+++ lib/kameleon/dsl/act.rb
@@ -10,22 +10,33 @@ module Kameleon
 
       def fill_in(fields)
         fields.each_pair do |value, selector|
-          session.fill_in selector, :with => value
+          case value
+            when :check
+              one_or_all(selector).each do |locator|
+                session.check locator
+              end
+            when :uncheck
+              one_or_all(selector).each do |locator|
+                session.uncheck locator
+              end
+            else
+              session.fill_in selector, :with => value
+          end
         end
       end
 
-      #      def click(link_text, options={:within => DEFAULT_AREA})
-      #  yield if block_given?
-      #  rspec_world.within(options[:within]) do
-      #    begin
-      #      rspec_world.click_on(self_or_translation_for(link_text))
-      #    rescue Capybara::ElementNotFound => e
-      #      attr_value, attr_type = within_value_and_type(options[:within])
-      #      xpath = attr_type ? "//*[@#{attr_type}='#{attr_value}']//*[*='%s']/*" : "//*[#{attr_type}]//*[*='%s']/*"
-      #      rspec_world.find(:xpath, xpath % self_or_translation_for(link_text)).click
-      #    end
-      #  end
-      #end
+#      def click(link_text, options={:within => DEFAULT_AREA})
+#  yield if block_given?
+#  rspec_world.within(options[:within]) do
+#    begin
+#      rspec_world.click_on(self_or_translation_for(link_text))
+#    rescue Capybara::ElementNotFound => e
+#      attr_value, attr_type = within_value_and_type(options[:within])
+#      xpath = attr_type ? "//*[@#{attr_type}='#{attr_value}']//*[*='%s']/*" : "//*[#{attr_type}]//*[*='%s']/*"
+#      rspec_world.find(:xpath, xpath % self_or_translation_for(link_text)).click
+#    end
+#  end
+#end
 
 
     end
--- spec/unit/act_spec.rb
+++ spec/unit/act_spec.rb
@@ -11,6 +11,30 @@ describe "Act" do
     <label for="xlInput">X-Large input</label>
     <input type="text" size="30" name="xlInput" id="xlInput" value="this is great value" class="xlarge">
 
+
+    <label for="prependedInput2">
+       Prepended Checkbox
+       <input type="checkbox" size="16" name="prependedInput2" id="prependedInput2" class="mini">
+   </label>
+    <label class="add-on"></label>
+
+
+    <label for="prependedInput3">Awesome Checkbox</label>
+    <input type="checkbox" size="16" name="prependedInput3" id="prependedInput3" class="mini">
+
+
+
+
+    <label for="appendedInput">Appended checkbox</label>
+    <label class="add-on active"><input type="checkbox" value="" id="appendedInput" name="">
+
+    <label for="some_checkbox_one">Some checkbox</label>
+    <input type="checkbox" checked="checked" value="" id="some_checkbox_one" name="">
+    <label for="some_checkbox_two">Another checkbox</label>
+    <input type="checkbox" checked="checked" value="" id="some_checkbox_two" name="">
+    <label for="some_checkbox_three">And One More Checkbox</label>
+    <input type="checkbox" checked="checked" value="" id="some_checkbox_three" name="">
+
     <input type="submit" value="Save changes" class="btn primary">
 
     <button class="btn" type="reset">Cancel</button>
@@ -35,7 +59,7 @@ describe "Act" do
 
   it "allow to chain many clicks - raise error, if at least one of them not found" do
     lambda {
-    @user.click "Save changes", "Load Next Page", "Submit", "Load Next Page"
+      @user.click "Save changes", "Load Next Page", "Submit", "Load Next Page"
     }.should raise_error(Capybara::ElementNotFound)
   end
 
@@ -50,6 +74,27 @@ describe "Act" do
       @user.not_see 'WoW' => 'xlInput'
       @user.fill_in 'WoW' => 'X-Large input'
       @user.see 'WoW' => 'xlInput'
+    end
+
+    context "check box" do
+      it "selecting" do
+        @user.will do
+          see :unchecked => ["Prepended Checkbox", "Appended checkbox", "Awesome Checkbox"]
+          fill_in :check => ["Appended checkbox", "Awesome Checkbox"]
+          session.save_and_open_page
+          see :checked => ["Appended checkbox", "Awesome Checkbox"],
+              :unchecked => "Prepended Checkbox"
+        end
+      end
+
+      it "unselecting" do
+        @user.will do
+          see :checked => ["Some checkbox", "Another checkbox", "And One More Checkbox"]
+          fill_in :uncheck => ["And One More Checkbox", "Some checkbox"]
+          see :unchecked => ["And One More Checkbox", "Some checkbox"],
+              :checked => "Another checkbox"
+        end
+      end
     end
   end
 end
