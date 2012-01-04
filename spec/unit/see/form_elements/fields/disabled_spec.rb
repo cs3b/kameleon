abf8cf1ea6caf5c097310aeb9fc5d8477166b624
Szymon Kieloch <skieloch@gmail.com>
2012-01-04 19:02:20 +0100

    Updated specs for check disabled fields.
    Removed checking if field is disabled and instead of this was added checking if field exists.

--- spec/unit/see/form_elements/fields/disabled_spec.rb
+++ spec/unit/see/form_elements/fields/disabled_spec.rb
@@ -7,22 +7,24 @@ describe 'see disabled fields' do
   end
 
   it 'should verify status for id' do
-    @user.see :disabled => 'disabledInput'
-    @user.not_see :disabled => 'xlInput'
+    @user.see :field => 'disabledInput',
+              :disabled => 'disabledInput'
   end
 
   it 'should verify status for label' do
-    @user.see :disabled => 'Disabled input'
-    @user.not_see :disabled => 'X-Large input'
+    @user.see :field => 'Disabled input',
+              :disabled => 'Disabled input'
   end
 
   context 'when many disabled fields at once' do
     it 'should verify status' do
-      @user.see :disabled => ['Disabled input', 'Disabled textarea']
+      @user.see :fields =>['Disabled input', 'Disabled textarea'],
+                :disabled => ['Disabled input', 'Disabled textarea']
     end
 
     context 'when at least one is enabled' do
       it 'should raise error' do
+        @user.see :fields => ['Disabled input', 'Disabled textarea', 'X-Large input']
         expect do
           @user.see :disabled => ['Disabled input', 'Disabled textarea', 'X-Large input']
         end.to raise_error(RSpec::Expectations::ExpectationNotMetError)
