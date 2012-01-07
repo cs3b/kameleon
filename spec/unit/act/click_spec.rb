5f9ea6df2126c8298f16779f1a0f15e8cba227ff
Szymon Kieloch <skieloch@gmail.com>
2012-01-07 14:53:16 +0100

    Refactoring specs for #click.
    Grouping examples by type of elements - buttons/links.

--- spec/unit/act/click_spec.rb
+++ spec/unit/act/click_spec.rb
@@ -6,36 +6,40 @@ describe '#click' do
     @user = Kameleon::User::Guest.new(self)
   end
 
-  it 'should click on link' do
-    @user.click 'Load Next Page'
-  end
-
-  it 'should click on button' do
-    @user.click 'Save changes'
-  end
-
-  it 'should click on cancel form' do
-    @user.click 'Cancel'
-  end
-
   it 'should clicks on links and buttons at once' do
     @user.click 'Save changes', 'Load Next Page', 'Save changes'
   end
 
-  it 'should click on button and accept pop-up' do
-    @user.click :and_accept => 'Confirm button'
+  context 'when buttons' do
+    it 'should click on button' do
+      @user.click 'Save changes'
+    end
+
+    it 'should click on cancel form' do
+      @user.click 'Cancel'
+    end
+
+    it 'should click with dismissing pop-up' do
+      @user.click :and_dismiss => 'Confirm button'
+    end
+
+    it 'should click with accepting pop-up' do
+      @user.click :and_accept => 'Confirm button'
+    end
   end
 
-  it 'should click on button and dismiss pop-up' do
-    @user.click :and_dismiss => 'Confirm button'
-  end
+  context 'when links' do
+    it 'should click on link' do
+      @user.click 'Load Next Page'
+    end
 
-  it 'should click on link and accept pop-up' do
-    @user.click :and_accept => 'Confirm link'
-  end
+    it 'should click with dismissing pop-up' do
+      @user.click :and_dismiss => 'Confirm link'
+    end
 
-  it 'should click on link and dismis pop-up' do
-    @user.click :and_dismiss => 'Confirm link'
+    it 'should click with accepting pop-up' do
+      @user.click :and_accept => 'Confirm link'
+    end
   end
 
   context 'when at least one does not exist' do
