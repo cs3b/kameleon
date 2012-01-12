85dfaf37734076a688f58d312288dfa7e297e652
Szymon Kieloch <skieloch@gmail.com>
2012-01-12 16:40:30 +0100

    Added spec for dragging many at once jquery eleements.

--- spec/unit/dsl/act/jquery-ui/sortable_elements/dragging_spec.rb
+++ spec/unit/dsl/act/jquery-ui/sortable_elements/dragging_spec.rb
@@ -7,16 +7,22 @@ describe '#drag jquery elements on sortable lists' do
   end
 
   context 'when target location is other element' do
+    it 'should dragging many at once' do
+      @user.see :ordered => ['Item 1', 'Item 2', 'Item 3', 'Item 4']
+      @user.drag 'Item 1' => { :before => 'Item 3' }, 'Item 4' => { :after => 'Item 2' }
+      @user.see :ordered => ['Item 2', 'Item 4', 'Item 1', 'Item 3']
+    end
+
     context 'when dragging element before other element' do
       it 'should dragging before element specific by label' do
         @user.see :ordered => ['Item 1', 'Item 2', 'Item 3', 'Item 4']
-        @user.drag 'Item 1', :before => 'Item 3'
+        @user.drag 'Item 1' => { :before => 'Item 3'  }
         @user.see :ordered => ['Item 2', 'Item 1', 'Item 3', 'Item 4']
       end
 
       it 'should dragging before element specific by id' do
         @user.see :ordered => ['Item 1', 'Item 2', 'Item 3', 'Item 4']
-        @user.drag 'item-1', :before => 'item-3'
+        @user.drag 'item-1' => { :before => 'item-3' }
         @user.see :ordered => ['Item 2', 'Item 1', 'Item 3', 'Item 4']
       end
     end
@@ -24,13 +30,13 @@ describe '#drag jquery elements on sortable lists' do
     context 'when dragging element after other element' do
       it 'should dragging after element specific by label' do
         @user.see :ordered => ['Item 1', 'Item 2', 'Item 3', 'Item 4']
-        @user.drag 'Item 1', :after => 'Item 2'
+        @user.drag 'Item 1' => { :after => 'Item 2' }
         @user.see :ordered => ['Item 2', 'Item 1', 'Item 3', 'Item 4']
       end
 
       it 'should dragging after element specific by id' do
         @user.see :ordered => ['Item 1', 'Item 2', 'Item 3', 'Item 4']
-        @user.drag 'item-1', :after => 'item-2'
+        @user.drag 'item-1' => { :after => 'item-2' }
         @user.see :ordered => ['Item 2', 'Item 1', 'Item 3', 'Item 4']
       end
     end
@@ -38,7 +44,7 @@ describe '#drag jquery elements on sortable lists' do
     context 'when target element does not exist' do
       it 'should raise error' do
         expect do
-          @user.drag 'item-1', :after => 'doesnotExist'
+          @user.drag 'item-1' => { :after => 'doesnotExist' }
         end.to raise_error(Capybara::ElementNotFound)
       end
     end
@@ -47,13 +53,13 @@ describe '#drag jquery elements on sortable lists' do
   context 'when target location is position on list' do
     it 'should dragging element to begin of list' do
       @user.see :ordered => ['Item 1', 'Item 2', 'Item 3', 'Item 4']
-      @user.drag 'item-3', :to => :begin
+      @user.drag 'item-3' => { :to => :begin }
       @user.see :ordered => ['Item 3', 'Item 1', 'Item 2', 'Item 4']
     end
 
     it 'should dragging element to end of list' do
       @user.see :ordered => ['Item 1', 'Item 2', 'Item 3', 'Item 4']
-      @user.drag 'item-3', :to => :end
+      @user.drag 'item-3' => { :to => :end }
       @user.see :ordered => ['Item 3', 'Item 1', 'Item 4', 'Item 3']
     end
   end
@@ -61,7 +67,7 @@ describe '#drag jquery elements on sortable lists' do
   context 'when sortable list is disabled' do
     it 'should not dragging element on sortable list' do
       @user.see :ordered => ['Disabled item 1', 'Disabled item 2', 'Disabled item 3', 'Disabled item 4']
-      @user.drag 'Disabled item 1', :to => :end
+      @user.drag 'Disabled item 1' => { :to => :end }
       @user.see :ordered => ['Disabled item 1', 'Disabled item 2', 'Disabled item 3', 'Disabled item 4']
     end
   end
