18abf51fbbd32cb161f5907f879cf523bb6893b5
Michal Czyz <michalczyz@gmail.com>
2012-01-19 15:28:04 +0100

    marking onhover tests as pending

--- lib/kameleon/dsl/act.rb
+++ lib/kameleon/dsl/act.rb
@@ -36,19 +36,6 @@ module Kameleon
         end
       end
 
-      #def click(link_text, options={:within => DEFAULT_AREA})
-      #  yield if block_given?
-      #  rspec_world.within(options[:within]) do
-      #    begin
-      #      rspec_world.click_on(self_or_translation_for(link_text))
-      #    rescue Capybara::ElementNotFound => e
-      #      attr_value, attr_type = within_value_and_type(options[:within])
-      #      xpath = attr_type ? "//*[@#{attr_type}='#{attr_value}']//*[*='%s']/*" : "//*[#{attr_type}]//*[*='%s']/*"
-      #      rspec_world.find(:xpath, xpath % self_or_translation_for(link_text)).click
-      #    end
-      #  end
-      #end
-
       def fill_in(locators)
         fill(locators) if can_fill?(locators)
       end
