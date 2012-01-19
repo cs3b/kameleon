c75aa434d64ad4bee2698db16503d4eca2fb9d8d
Michal Czyz <michalczyz@gmail.com>
2012-01-19 15:13:38 +0100

    rspec cleanup and optimalization

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,15 @@ require 'kameleon'
 require 'capybara/dsl'
 require 'capybara/rspec/matchers'
 
+Dir["#{File.expand_path("../", __FILE__)}/support/**/*.rb"].each { |f| require f }
+
 require 'sample_rack_app/hey'
+
+RSpec.configure do |config|
+  config.before(:all) do
+    DeferredGarbageCollection.start
+  end
+  config.after(:all) do
+    DeferredGarbageCollection.reconsider
+  end
+end
