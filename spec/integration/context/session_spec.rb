1fd28343b989af5e40f12407ea758728d779c7df
Michał Czyż <michalczyz@gmail.com>
2012-02-25 13:48:55 +0100

    spec for method chain on session

--- spec/integration/context/session_spec.rb
+++ spec/integration/context/session_spec.rb
@@ -1,28 +1,39 @@
 require 'spec_helper'
 
 describe "Session" do
-  it "always available - possible to switch between sessions if you know id" do
-    # we use :default session
-    load_page('/texts')
+  describe "switch" do
+    before do
+      # we use :default session
+      load_page('/texts')
 
-    # create new session with identifier ':user'
-    create_session(:user)
-    load_page
-
-    # switch session
-    act_as(:default)
-    see 'This is simple app', 'in that app'
-    unseeing 'Home'
-
-    # swith session temporary (inside block)
-    act_as(:user) do
-      unseeing 'This is simple app', 'in that app'
-      see 'Home'
+      # create new session with identifier ':user'
+      create_session(:user)
+      load_page
     end
 
-    # block should not change session globally
-    see 'This is simple app', 'in that app'
-    unseeing 'Home'
+    it "always available - possible to switch between sessions if you know id" do
+      # switch session
+      act_as(:default)
+      see 'This is simple app', 'in that app'
+      unseeing 'Home'
+
+      # swith session temporary (inside block)
+      act_as(:user) do
+        unseeing 'This is simple app', 'in that app'
+        see 'Home'
+      end
+
+      # block should not change session globally
+      see 'This is simple app', 'in that app'
+      unseeing 'Home'
+    end
+
+    it "temporary switch session for single method call" do
+      act_as(:user).see 'Home'
+      act_as(:default).see 'in that app'
+      act_as(:user).unseeing 'in that app'
+      act_as(:default).unseeing 'Home'
+    end
   end
 
   describe "Drivers" do
