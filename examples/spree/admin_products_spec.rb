25fb41e7b8b8a54505e74458534100c9819a40bb
Michał Czyż <michalczyz@gmail.com>
2012-03-02 13:24:18 +0100

    updating part of example to 0_2_0 format

--- examples/spree/admin_products_spec.rb
+++ examples/spree/admin_products_spec.rb
@@ -4,9 +4,8 @@ require 'spec_helper'
 
 feature "Products", :driver => :selenium do
   background do
-    @admin = TestUserAdministrator.new(self) do
-      visit spree.admin_path
-    end
+    signin_as(:admin)
+    load_page spree.admin_path
   end
 
   context "listing products" do
@@ -14,11 +13,11 @@ feature "Products", :driver => :selenium do
       Factory(:product, :name => 'apache baseball cap', :available_on => '2011-01-06 18:21:13:', :count_on_hand => '0')
       Factory(:product, :name => 'zomg shirt', :available_on => '2125-01-06 18:21:13', :count_on_hand => '5')
 
-      @admin.click "Products"
-      @admin.see :ordered => ["apache baseball cap", "zomg shirt" ]
-      @admin.click "admin_products_listing_name_title"
-      @admin.see :ordered =>  ["zomg shirt", "apache baseball cap"  ]
+      click "Products"
+      see :ordered => ["apache baseball cap", "zomg shirt"]
 
+      click "admin_products_listing_name_title"
+      see :ordered => ["zomg shirt", "apache baseball cap"]
     end
   end
 
@@ -27,19 +26,19 @@ feature "Products", :driver => :selenium do
       Factory(:product, :name => 'apache baseball cap', :available_on => '2011-01-06 18:21:13:', :deleted_at => "2011-01-06 18:21:13")
       Factory(:product, :name => 'zomg shirt', :available_on => '2125-01-06 18:21:13')
 
-      @admin.click "Products"
-      @admin.see "zomg shirt"
-      @admin.not_see "apache baseball cap"
+      click "Products"
+      see "zomg shirt"
+      unseeing "apache baseball cap"
 
-      @admin.fill_in(:check => "Show Deleted")
-      @admin.click "Search"
-      @admin.see "zomg shirt" , "apache baseball cap"
+      fill_in(:check => "Show Deleted")
+      click "Search"
+      see "zomg shirt", "apache baseball cap"
 
-      @admin.fill_in(:uncheck => "Show Deleted")
-      @admin.click "Search"
+      fill_in(:uncheck => "Show Deleted")
+      click "Search"
 
-      @admin.see "zomg shirt"
-      @admin.not_see "apache baseball cap"
+      see "zomg shirt"
+      unseeing "apache baseball cap"
     end
 
     it "should be able to search products by their properties" do
@@ -49,16 +48,16 @@ feature "Products", :driver => :selenium do
       Spree::Product.update_all :count_on_hand => 10
 
       @admin.click "Products"
-      @admin.fill_in( "ap" => "search_name_contains")
+      @admin.fill_in("ap" => "search_name_contains")
 
       @admin.click "Search"
       @admin.see "apache baseball cap", "apache baseball cap2"
       @admin.not_see "zomg shirt"
 
-      @admin.fill_in( "A1" => "search_variants_including_master_sku_contains")
+      @admin.fill_in("A1" => "search_variants_including_master_sku_contains")
       @admin.click "Search"
 
-      @admin.see  "apache baseball cap"
+      @admin.see "apache baseball cap"
       @admin.not_see "apache baseball cap2", "zomg shirt"
 
     end
@@ -67,23 +66,23 @@ feature "Products", :driver => :selenium do
   context "creating a new product" do
 
     background do
-     @admin.click  "Products", "admin_new_product"
-     @admin.within('#new_product') { see "SKU" }
+      click "Products", "admin_new_product"
+      within('#new_product') { see "SKU" }
     end
 
     it "should allow an admin to create a new product", :js => true do
-      @admin.fill_in("Baseball Cap" => "product_name" )
-      @admin.fill_in("B100" => "product_sku" )
-      @admin.fill_in("100" => "product_price" )
-      @admin.fill_in("2012/01/24" => "product_available_on" )
+      fill_in "Baseball Cap" => "product_name",
+              "B100" => "product_sku",
+              "100" => "product_price",
+              "2012/01/24" => "product_available_on"
 
-      @admin.click "Create"
+      click "Create"
 
-      @admin.see "successfully created!"
-      @admin.fill_in("100" => "product_on_hand")
+      see "successfully created!"
+      fill_in "100" => "product_on_hand"
 
-      @admin.click "Update"
-      @admin.see "successfully updated!"
+      click "Update"
+      see "successfully updated!"
     end
 
     it "should show validation errors", :js => true do
@@ -96,7 +95,7 @@ feature "Products", :driver => :selenium do
     it "should allow an admin to clone a product" do
       Factory(:product, :name => 'apache baseball cap', :available_on => '2011-01-01 01:01:01', :sku => "A100")
 
-      @admin.click  "Products"
+      @admin.click "Products"
 
       @admin.within('table#listing_products tr:nth-child(2)') { click "Clone" }
       @admin.see "Product has been cloned"
@@ -112,15 +111,15 @@ feature "Products", :driver => :selenium do
 
       @admin.click "Products"
       @admin.within('table#listing_products tr:nth-child(2)') { click "Edit" }
-      @admin.click  "Images", "new_image_link"
+      @admin.click "Images", "new_image_link"
 
       absolute_path = File.expand_path(Rails.root.join('..', '..', 'spec', 'support', 'ror_ringer.jpeg'))
-      @admin.fill_in(:attach => ['image_attachment', absolute_path] )
+      @admin.fill_in(:attach => ['image_attachment', absolute_path])
 
       @admin.click "Update"
       @admin.see "successfully created!"
 
-      @admin.within('table.index tr:nth-child(2)') { click  "Edit" }
+      @admin.within('table.index tr:nth-child(2)') { click "Edit" }
       @admin.fill_in("ruby on rails t-shirt" => "image_alt")
 
       @admin.click "Update"
