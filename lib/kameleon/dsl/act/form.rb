78b1f5e0383c695cc4bda0d28267e91f6614124c
Michał Czyż <michalczyz@gmail.com>
2012-03-08 14:46:06 +0100

    cleanup act - form

--- lib/kameleon/dsl/act/form.rb
+++ lib/kameleon/dsl/act/form.rb
@@ -103,37 +103,4 @@ module Kameleon
   end
 end
 
-#def can_fill?(locators)
-#  locators.values.each do |loc|
-#    selectors = loc.respond_to?(:values) ? loc.values : loc
-#    one_or_all(selectors).each do |selector|
-#      return false if unmodifiable?(selector)
-#    end
-#  end
-#end
-#
-#def unmodifiable?(locator)
-#  field = session.find_field(locator)
-#  case session.driver
-#    when Capybara::Selenium::Driver
-#      # In driver field attributes - disabled and readonly may be:
-#      # 1) string - false or true
-#      # 2) nil value
-#      return eval(field[:disabled].to_s) || eval(field[:readonly].to_s)
-#    when Capybara::RackTest::Driver
-#      # In rack field attributes - disabled and readonly may be:
-#      # 1) nil value
-#      # 2) string - disabled or readonly
-#      return field[:disabled] || field[:readonly]
-#  end
-#end
-#
-#def get_full_test_asset_path(file_path)
-#  if File.file?(file_path)
-#    file_path
-#  elsif File.file?(Helpers.default_path_for_file(file_path))
-#    Helpers.default_path_for_file(file_path)
-#  else
-#    raise "Sorry but we didn't found that file in: #{file_path}, neither #{Helpers.default_path_for_file(file_path)}"
-#  end
-#end
+#! in future should we check in RackTest - does field is editable (detect readonly, disabled attributes) ?
