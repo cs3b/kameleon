283b2ad8da30d79480ef4d7a303bbc421d4d32ea
Michal Czyz <michalczy@gmail.com>
2013-04-06 19:48:18 +0200

    and one more time

--- script/travisci/bundle_cache.sh
+++ script/travisci/bundle_cache.sh
@@ -3,7 +3,7 @@
 if [ $CACHE_BUNDLE -eq 1 ]; then
 rm -rf bundle.tgz
     tar -czf bundle.tgz .bundle
-    scp -c blowfish -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q\
+    scp -c blowfish -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q \
     bundle.tgz travisci_kameleon@selleo.com:/home/travisci_kameleon/bundle.tgz
 fi
 
